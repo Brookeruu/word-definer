642751b17babb55abd63d1ca4211ded17dedd4c7
Brooke Perry <perrysbrooke@gmail.com>
2018-11-30 11:12:15 -0800

    create a new word

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,9 +3,24 @@ require 'pry'
 require 'word'
 
 describe(Word) do
-  describe('initialize') do
+
+  before() do
+    Word.clear
+  end
+
+  describe('.all') do
     it('will display dictionary as empty array') do
       expect(Word.all).to(eq([]))
     end
   end
+
+  describe('.initialize') do
+    it('create a new word') do
+      new_word = Word.new({:word=> 'dog', :definition=> 'woof'})
+      expect(new_word.word).to(eq('dog'))
+      expect(new_word.definition).to(eq(['woof']))
+    end
+  end
+
+
 end
