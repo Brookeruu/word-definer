4edb8b2ef8b3233317a66ccd85909831299d6797
Brooke Perry <perrysbrooke@gmail.com>
2018-11-30 23:06:56 -0800

    add integration test

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -2,3 +2,26 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('word definer', {:type => :feature}) do
+
+  it('display the input user word') do
+    visit('/')
+    fill_in('word', :with => "sample")
+    click_button('Add word')
+    expect(page).to have_content("sample")
+  end
+
+  it('display the input user definition') do
+    visit('/output/:id')
+    fill_in('definition', :with => "sample definition")
+    click_button('Add definition')
+    expect(page).to have_content("sample definition")
+  end
+end
