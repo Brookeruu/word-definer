9a7f2985e46a306e9a96b27a7edcd83dbf8049c1
Brooke Perry <perrysbrooke@gmail.com>
2018-12-08 13:11:35 -0800

    update int_spec to show original input def and new def

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,12 +3,12 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# before(:each) do
-#   visit('/')
-#   fill_in('word', :with => "sample")
-#   fill_in('definition', :with => "samples")
-#   save_and_open_page
-# end
+before(:each) do
+  visit('/')
+  fill_in('word', :with => "sample")
+  fill_in('definition', :with => "samples")
+  save_and_open_page
+end
 
 describe('word definer', {:type => :feature}) do
   it('display the input user word') do
@@ -17,11 +17,6 @@ describe('word definer', {:type => :feature}) do
     click_button('Add word')
     expect(page).to have_content("sample")
   end
-  #
-  # it('display input user definition') do
-  #   click_button('Add word')
-  #   expect(page).to have_content("sample")
-  # end
 
   it('display the input user definition') do
     test_word = Word.new({:word => "example", :definition => "example definition"})
@@ -30,6 +25,7 @@ describe('word definer', {:type => :feature}) do
     click_link('example')
     fill_in('definition', :with => "sampler")
     click_button('Add definition')
+    expect(page).to have_content("example definition")
     expect(page).to have_content("sampler")
   end
 end
